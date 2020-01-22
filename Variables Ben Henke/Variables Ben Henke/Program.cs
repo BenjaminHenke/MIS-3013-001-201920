4490c5ec557629c8d4edba562e7ec3337a37f5a5
henk0000 <henk0000@PCB-LAB360-05>
2020-01-22 12:54:11 -0600

    Created variable for first name, last name, favorite number

--- Variables Ben Henke/Variables Ben Henke/Program.cs
+++ Variables Ben Henke/Variables Ben Henke/Program.cs
@@ -11,6 +11,28 @@ namespace Variables_Ben_Henke
     {
         static void Main(string[] args)
         {
+            //create an integar name firstName
+            string firstName;
+
+
+            //Assign a value to firstName (your name)
+            firstName = "Ben";
+
+            //Create an int named favoriteNumber and assign your favorite number to it
+            int favoriteNumber = 7;
+
+            double crazyNumber = 7.6767 * favoriteNumber;
+
+            string lastName = "Henke";
+            string fullName = firstName + lastName;
+
+            Console.WriteLine(fullName + "has the favorite number of" + favoriteNumber +  "\nwhich equates to a crazy number of" + crazyNumber);
+
+
+            //Make it Dynamic
+
+
+            Console.ReadKey();
         }
     }
 }
