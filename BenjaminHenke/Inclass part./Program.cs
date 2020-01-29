8344c80cc8df6d8288793bb4c78cdc7515779ec5
henk0000 <henk0000@PCB-LAB360-05>
2020-01-29 12:52:45 -0600

    Inclass participation

---
+++ BenjaminHenke/Inclass part./Program.cs
@@ -0,0 +1,42 @@
+﻿//Benjamin Henke
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inclass_part
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int coin;// this will hold my random int numbers.
+
+            string userInput;// This will hold all my user input/ answers.
+
+
+
+
+            Console.WriteLine("Hello, Pick Heads or Tails:");
+            string UserInputAsString = console.readline();
+            int UserInput;
+
+            if (UserInputAsString == 'Heads')
+            {
+                UserInput = 'Heads';
+            }
+            else
+            { UserInput = 'Tails';
+                 
+            random read = new Random();
+
+                int randomNumber = rad.next(0, 2);
+
+            if (UserInputAsString == 'Trails')
+                Console.ReadKey();
+
+        
+        }
+    }
+}
