0073658c1ed485fa64b882f54700dbd48ab71e02
henk0000 <henk0000@PCB-LAB360-05>
2020-02-03 13:05:52 -0600

    Loops

---
+++ Loop_summ/Loop_summ/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loop_summ
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("what number do you want to count up to?");
+            int threshold = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 1; i <= threshold; i++)
+            {
+                Console.WriteLine(i + " ");
+            }
+
+        
+            Console.ReadKey();
+        }
+
+    }
+}
